a930393dabba34d23242da807d1a9e5ef484aac9
SHASHI PREETHAM CHOLLURI <121763930+shashipreetham-4@users.noreply.github.com>
2025-01-16 23:20:38 +0530

    Time: 0 ms (100.00%) | Memory: 31.9 MB (60.52%) - LeetSync

---
+++ 3708-zigzag-grid-traversal-with-skip/zigzag-grid-traversal-with-skip.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    vector<int> zigzagTraversal(vector<vector<int>>& grid) 
+    {
+        vector<int> ans;
+
+        for(int r = 0 ; r < grid.size();r++)
+            {
+                if(r%2==0)
+                {
+                    for(int c = 0 ; c < grid[0].size() ; c++)
+                        if(c%2==0) ans.push_back(grid[r][c]); 
+                }
+                else
+                {
+                   for(int c = grid[0].size()-1 ; c >=0 ; c--)
+                        if(c%2==1) ans.push_back(grid[r][c]); 
+                }
+            }
+        return ans;
+    }
+};
